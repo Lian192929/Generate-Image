5deaa5a1560af148c83b488169537e1b4c23a309
Lian <115387768+Lian192929@users.noreply.github.com>
2024-07-11 14:05:55 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const apiKey = "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";
+const apiKey = "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";
 
 const maxImages = 4; // Number of images to generate for each prompt
 let selectedImageNumber = null;
